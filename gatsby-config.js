96a237a841af5cbd5b1caa365592d6cd1da1ec26
unknown <divya.zestard@gmail.com>
2020-06-09 11:39:50 +0530

    Configure wordpress with gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -26,6 +27,40 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+         */
+        baseUrl: "http://phptasks.com/iscriptics",
+        // The protocol. This can be http or https.
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on wordpress.com.
+        // Defaults to true.
+        auth: {
+          wpcom_user: "admin",
+          wpcom_pass: '#8SHlvNk%(p8N)LVkX',
+        },
+        useACF: true,
+        perPage: 100,
+        concurrentRequests: 10,
+        keepMediaSizes: false,
+        normalizer: function ({ entities }) {
+          return entities
+        },
+        // searchAndReplaceContentUrls: {
+        //   sourceUrl: "http://finntia.phptasks.com/",
+        //   replacementUrl: "https://finntia.netlify.com/",
+        // },
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
