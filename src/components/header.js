b5df6e24dc3173098dd32820fb7a6a10c707d9f5
unknown <divya.zestard@gmail.com>
2020-06-26 11:18:35 +0530

    Full page slide

--- src/components/header.js
+++ src/components/header.js
@@ -63,66 +63,7 @@ const Header = () => {
   
   return(
   <header>
-    <nav className="navbar navbar-default navbar-fixed-top scrollbg-show" role="navigation">
-      <div className="container mt45">
-          <div className="navbar-header page-scroll">
-              <a className="navbar-brand" href="#">
-                <img src={Sitelogo} /> 
-              </a>
-          </div>
-          <div className="header-menu hidden-tab">
-              <ul className="nav navbar-nav navbar-right pos-right">
-                      <li> <a href="#" title="Careers">Careers</a> </li>
-                      <li> <a href="#" title="Investors">Investors</a> </li>
-                      <li> <a href="#" title="Navigate your next">Navigate your next</a> </li>
-              </ul>
-          </div>
-          <div className="search__color">
-              <svg className="hidden1">
-                  <defs>
-                      <symbol id="icon-search" viewBox="0 0 24 24">
-                          <title>search</title>
-                          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                      </symbol>
-                      <symbol id="icon-cross" viewBox="0 0 24 24">
-                          <title>cross</title>
-                          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-                      </symbol>
-                  </defs>
-              </svg>
-              <main className="main-wrap">
-                  <div className="search-wrap search-icon">
-                      <button id="btn-search" className="btn1 btn--search">
-                          {/* <svg className="icon icon--search">
-                                  <use xlink:href="#icon-search"></use>
-                          </svg> */}
-                      </button>
-                  </div>
-              </main>
-              <div className="search">
-                  <button id="btn-search-close" className="btn1 btn--search-close" aria-label="Close search form">
-                      {/* <svg className="icon icon--cross">
-                          <use xlink:href="#icon-cross"></use>
-                      </svg> */}
-                  </button>
-                  <div className="search__inner search__inner--up">
-                      {/* action needs to be attached */}
-                      <form className="search__form" action="/search.html" method="get">
-                          <input className="search__input" name="k" type="search" placeholder="What are you looking for?" autocomplete="off" spellcheck="false"/>
-                          <span className="search__info">Hit enter to search or ESC to close</span>
-                      </form>
-                  </div>
-                  <div className="search__inner search__inner--down">
-                      {/* search related content */}
-                      <div className="search__related"></div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="progressbar">
-          <div className="width"></div>
-      </div>
-    </nav>
+   <h1>Header</h1>
   </header>
   )
 }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,37 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
-
+import {SectionsContainer, Section} from 'react-fullpage';
+class SecondPage extends Component {
+  render() {
+    let options = {
+      sectionClassName:     'section',
+      anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
+      scrollBar:            false,
+      navigation:           true,
+      verticalAlign:        false,
+      sectionPaddingTop:    '50px',
+      sectionPaddingBottom: '50px',
+      arrowNavigation:      true
+    };
+ 
+    return (
+      <Layout>
+<div>
+        
+        <SectionsContainer className="container" {...options}>
+          <Section className="custom-section" verticalAlign="true" color="#69D2E7">Page 1</Section>
+          <Section color="#A7DBD8">Page 2</Section>
+          <Section color="#E0E4CC">Page 3</Section>
+        </SectionsContainer>
+      </div>
+      </Layout>
+      
+    );
+  }
+}
 export default SecondPage
+
